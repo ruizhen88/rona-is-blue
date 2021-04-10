cedc402d3738b332ee42d6a5a9b325ef68431bc3
yejin_workmate <yejin@workmate.asia>
2021-04-11 00:21:14 +0900

    init project

---
+++ src/models/question.model.js
@@ -0,0 +1,102 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { toJSON, paginate } = require('./plugins');
+const { roles } = require('../config/roles');
+
+const questionSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    // TODO: add later
+    // categories: {
+    //   type: Array,
+    // },
+    // answer: {
+    //   type: Object,
+    // },
+    body: {
+      type: String,
+      required: true,
+    },
+    // email: {
+    //   type: String,
+    //   required: true,
+    //   unique: true,
+    //   trim: true,
+    //   lowercase: true,
+    //   validate(value) {
+    //     if (!validator.isEmail(value)) {
+    //       throw new Error('Invalid email');
+    //     }
+    //   },
+    // },
+    // password: {
+    //   type: String,
+    //   required: true,
+    //   trim: true,
+    //   minlength: 8,
+    //   validate(value) {
+    //     if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+    //       throw new Error('Password must contain at least one letter and one number');
+    //     }
+    //   },
+    //   private: true, // used by the toJSON plugin
+    // },
+    // role: {
+    //   type: String,
+    //   enum: roles,
+    //   default: 'question',
+    // },
+    // isEmailVerified: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+questionSchema.plugin(toJSON);
+questionSchema.plugin(paginate);
+
+/**
+ * Check if email is taken
+ * @param {string} email - The question's email
+ * @param {ObjectId} [excludeQuestionId] - The id of the question to be excluded
+ * @returns {Promise<boolean>}
+ */
+// questionSchema.statics.isEmailTaken = async function (email, excludeQuestionId) {
+//   const question = await this.findOne({ email, _id: { $ne: excludeQuestionId } });
+//   return !!question;
+// };
+
+/**
+ * Check if password matches the question's password
+ * @param {string} password
+ * @returns {Promise<boolean>}
+ */
+// questionSchema.methods.isPasswordMatch = async function (password) {
+//   const question = this;
+//   return bcrypt.compare(password, question.password);
+// };
+
+// questionSchema.pre('save', async function (next) {
+//   const question = this;
+//   if (question.isModified('password')) {
+//     question.password = await bcrypt.hash(question.password, 8);
+//   }
+//   next();
+// });
+
+/**
+ * @typedef Question
+ */
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
---
+++ src/routes/v1/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const authRoute = require('./auth.route');
+const userRoute = require('./user.route');
+const questionRoute = require('./question.route');
+const docsRoute = require('./docs.route');
+const config = require('../../config/config');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/users',
+    route: userRoute,
+  },
+  {
+    path: '/questions',
+    route: questionRoute,
+  },
+];
+
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
+  {
+    path: '/questions',
+    route: questionRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
+
+module.exports = router;
