e5e9a8caf82e08fcabeb3064d72ec993ba37911b
ruizhen88 <yejin@workmate.asia>
2021-07-25 19:24:33 +0900

    added movie modules to index

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Question = require('./question.model');
 module.exports.Answer = require('./answer.model');
+module.exports.Movie = require('./movie.model');
--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,4 @@ module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
 module.exports.questionService = require('./question.service');
 module.exports.answerService = require('./answer.service');
+module.exports.movieService = require('./movie.service');
