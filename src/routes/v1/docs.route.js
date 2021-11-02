953a3aeeb9d4abab486d28ba4315e350c65a9024
ruizhen88 <yejin@workmate.asia>
2021-11-02 13:01:32 +0900

    add: docs for posts

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -7,7 +7,8 @@ const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  // apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  apis: ['src/docs/*.yml', 'src/routes/v1/post.route.js'],
 });
 
 router.use('/', swaggerUi.serve);
--- src/routes/v1/post.route.js
+++ src/routes/v1/post.route.js
@@ -11,197 +11,178 @@ router.route('/:postId').get(postController.getPost).patch(postController.update
 
 module.exports = router;
 
-// /**
-//  * @swagger
-//  * tags:
-//  *   name: Posts
-//  */
+/**
+ * @swagger
+ * tags:
+ *   name: Posts
+ */
 
-// /**
-//  * @swagger
-//  * /posts:
-//  *   post:
-//  *     summary: Create a post
-//  *     description:
-//  *     tags: [Posts]
-//  *     security:
-//  *       - bearerAuth: []
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             type: object
-//  *             required:
-//  *               - title
-//  *               - body
-//  *               - category
-//  *             properties:
-//  *               title:
-//  *                 type: string
-//  *               body:
-//  *                 type: string
-//  *               category:
-//  *                 type: string
-//  *             example:
-//  *               title: 알약 포장재
-//  *               body: 알약 포장재는 어떻게 배출하나요?
-//  *               category: 플라스틱
-//  *     responses:
-//  *       "201":
-//  *         description: Created
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *                $ref: '#/components/schemas/Post'
-//  *
-//  *   get:
-//  *     summary: Get all posts
-//  *     description:
-//  *     tags: [Posts]
-//  *     security:
-//  *       - bearerAuth: []
-//  *     parameters:
-//  *       - in: query
-//  *         name: title
-//  *         schema:
-//  *           type: string
-//  *         description: Post title
-//  *       - in: query
-//  *         name: body
-//  *         schema:
-//  *           type: string
-//  *         description: Post body
-//  *       - in: query
-//  *         name: category
-//  *         schema:
-//  *           type: string
-//  *         description: Post category
-//  *       - in: query
-//  *         name: sortBy
-//  *         schema:
-//  *           type: string
-//  *         description: sort by query in the form of field:desc/asc (ex. title:asc)
-//  *       - in: query
-//  *         name: limit
-//  *         schema:
-//  *           type: integer
-//  *           minimum: 1
-//  *         default: 10
-//  *         description: Maximum number of posts
-//  *       - in: query
-//  *         name: page
-//  *         schema:
-//  *           type: integer
-//  *           minimum: 1
-//  *           default: 1
-//  *         description: Page number
-//  *     responses:
-//  *       "200":
-//  *         description: OK
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *               type: object
-//  *               properties:
-//  *                 results:
-//  *                   type: array
-//  *                   items:
-//  *                     $ref: '#/components/schemas/Post'
-//  *                 page:
-//  *                   type: integer
-//  *                   example: 1
-//  *                 limit:
-//  *                   type: integer
-//  *                   example: 10
-//  *                 totalPages:
-//  *                   type: integer
-//  *                   example: 1
-//  *                 totalResults:
-//  *                   type: integer
-//  *                   example: 1
-//  */
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - body
+ *             properties:
+ *               title:
+ *                 type: string
+ *               body:
+ *                 type: string
+ *               tags:
+ *                 type: array
+ *             example:
+ *               title: 웹 개발자 포트폴리오 A to Z(기획부터 배포까지)
+ *               body: 1. IT 회사에 개발자 취업하는 방법\nIT 회사에 개발자로 취업을 하려면 개발에 대한 지식 / 경력 / 실력이 필요합니다. 경력이 없는 신입(~3년 차)에게 회사가 지식과 경력과 실력을 확인하는 방법은 무엇일까요?
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Post'
+ *
+ *   get:
+ *     summary: Get all posts
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: query
+ *         name: title
+ *         schema:
+ *           type: string
+ *         description: Post title
+ *       - in: query
+ *         name: body
+ *         schema:
+ *           type: string
+ *         description: Post body
+ *       - in: query
+ *         name: tags
+ *         schema:
+ *           type: string
+ *         description: Post tags
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *         description: sort by query in the form of field:desc/asc (ex. title:asc)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of posts
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Post'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 1
+ *                 totalResults:
+ *                   type: integer
+ *                   example: 1
+ */
 
-// /**
-//  * @swagger
-//  * /posts/{id}:
-//  *   get:
-//  *     summary: Get a post
-//  *     description:
-//  *     tags: [Posts]
-//  *     security:
-//  *       - bearerAuth: []
-//  *     parameters:
-//  *       - in: path
-//  *         name: id
-//  *         required: true
-//  *         schema:
-//  *           type: string
-//  *         description: Post id
-//  *     responses:
-//  *       "200":
-//  *         description: OK
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *                $ref: '#/components/schemas/Post'
-//  *
-//  *   patch:
-//  *     summary: Update a post
-//  *     description:
-//  *     tags: [Posts]
-//  *     security:
-//  *       - bearerAuth: []
-//  *     parameters:
-//  *       - in: path
-//  *         name: id
-//  *         required: true
-//  *         schema:
-//  *           type: string
-//  *         description: Post id
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             type: object
-//  *             properties:
-//  *               title:
-//  *                 type: string
-//  *               body:
-//  *                 type: string
-//  *               category:
-//  *                 type: string
-//  *             example:
-//  *               title: 알약 포장재 (수정)
-//  *               body: 알약 포장재는 어떻게 배출하나요? (수정)
-//  *               category: 플라스틱 (수정)
-//  *     responses:
-//  *       "200":
-//  *         description: OK
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *                $ref: '#/components/schemas/Post'
-//  *       "404":
-//  *         $ref: '#/components/responses/NotFound'
-//  *
-//  *   delete:
-//  *     summary: Delete a post
-//  *     description:
-//  *     tags: [Posts]
-//  *     security:
-//  *       - bearerAuth: []
-//  *     parameters:
-//  *       - in: path
-//  *         name: id
-//  *         required: true
-//  *         schema:
-//  *           type: string
-//  *         description: Post id
-//  *     responses:
-//  *       "200":
-//  *         description: No content
-//  *       "404":
-//  *         $ref: '#/components/responses/NotFound'
-//  */
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get a post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Post id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Post'
+ *
+ *   patch:
+ *     summary: Update a post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Post id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               body:
+ *                 type: string
+ *               tags:
+ *                 type: array
+ *             example:
+ *               tags: [리액트, 스프링, 웹개발자]
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Post'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Post id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
